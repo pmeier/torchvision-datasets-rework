8ce084c8a2d4e863f7c5fc8ea00245bb31f31750
Vincent Moens <vmoens@fb.com>
2021-08-20 11:16:54 +0100

    high-level dispatching transform pipeline

--- torchvision/transforms/_core.py
+++ torchvision/transforms/_core.py
@@ -2,6 +2,7 @@ import collections.abc
 import inspect
 import re
 from typing import Any, Callable, Dict, Iterator, Optional, Type, TypeVar
+from warnings import warn
 
 import torch
 from torch import nn
@@ -23,6 +24,17 @@ def query_sample(sample: Any, fn: Callable[[Any], Optional[T]]) -> Iterator[T]:
             yield result
 
 
+class TransformDispatch():
+    def __init__(cls, transforms):
+        cls.transforms = transforms
+        cls.transforms.set_reset_auto(False)
+
+    def __call__(cls, input_dict):
+        input_dict = {key: cls.transform(value) for key, value in input_dict.items()}
+        cls.transform.wipeout_()
+        return input_dict
+
+
 class Transform(nn.Module):
     _KNOWN_FEATURE_TYPES = {
         "_".join([part.lower() for part in re.findall("[A-Z][^A-Z]*", feature_type.__name__)]): feature_type
@@ -30,9 +42,18 @@ class Transform(nn.Module):
     }
 
     def __init_subclass__(cls, *, auto_register: bool = True):
+        cls.initialized = False
+        cls.reset_auto = True
         cls._feature_transforms: Dict[Type[Feature], Callable] = {}
         if auto_register:
             cls._auto_register()
+
+    def set_reset_auto(cls, mode=True):
+        cls.reset_auto = mode
+    
+    def wipeout_(cls):
+        cls.initialized = False
+        cls.saved_params = None
 
     @classmethod
     def _auto_register(cls) -> None:
@@ -78,35 +99,43 @@ class Transform(nn.Module):
         feature_transform = cls._feature_transforms[feature_type]
         return feature_transform(input, **params)
 
-    def get_params(self, sample: Any) -> Dict[str, Any]:
+    def get_params(cls, sample: Any) -> Dict[str, Any]:
         return dict()
 
-    def forward(self, *inputs: Any, params: Optional[Dict[str, Any]] = None) -> Any:
-        sample = inputs if len(inputs) > 1 else inputs[0]
-        if params is None:
-            params = self.get_params(sample)
+    def forward(cls, input: torch.Tensor) -> torch.Tensor:
+        if not cls.initialized:
+            cls.saved_params = cls.get_params(input)
 
-        def apply(sample: Any):
-            if isinstance(sample, collections.abc.Sequence):
-                return [apply(item) for item in sample]
-            elif isinstance(sample, collections.abc.Mapping):
-                return {name: apply(item) for name, item in sample.items()}
-            else:
-                feature_type = type(sample)
-                if not (feature_type is torch.Tensor or feature_type in self._feature_transforms):
-                    return sample
+        feature_type = type(input)
+        if not (feature_type is torch.Tensor or feature_type in cls._feature_transforms):
+            return input
 
-                return self.apply(sample, **params)
-
-        return apply(sample)
+        output = cls.apply(input, **cls.saved_params)
+        if cls.reset_auto:
+            cls.wipeout_()
+        return output
 
 
 class Compose(nn.Module):
-    def __init__(self, *transforms: Transform) -> None:
+    def __init__(cls, *transforms: Transform) -> None:
         super().__init__()
-        self._transforms = transforms
+        cls._transforms = transforms
 
-    def forward(self, *inputs: Any) -> Any:
-        for transform in self._transforms:
+    def forward(cls, *inputs: Any) -> Any:
+        for transform in cls._transforms:
             inputs = transform(*inputs)
         return inputs
+
+    def set_reset_auto(cls, mode=True):
+        for t in cls._transforms:
+            if not isinstance(t, (Compose, Transform)):
+                warn(f"transform of type {t} cannot be set to reset_auto={mode} as it is not a Transform instance")
+            else:
+                t.set_reset_auto(mode)
+    
+    def wipeout_(cls):
+        for t in cls._transforms:
+            if not isinstance(t, (Compose, Transform)):
+                warn(f"transform of type {t} cannot be wiped out as it is not a Transform instance")
+            else:
+                t.wipeout_()
